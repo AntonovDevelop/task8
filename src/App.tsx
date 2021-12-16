779ced8e148adbe4d90ca842fe378aba87118d9f
AntonovDeveloper <antonov04092002@mail.ru>
2021-12-16 11:44:28 +0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {  AppBar,  Toolbar,  Typography,  Card,  CardContent,  Grid} from "@mui/material";
 
+interface GithubRepo {
+  id: number;
+  name: string;
+  description: string;
+}
+
+const getData = async () => {
+  return await fetch(`https://api.github.com/users/vladdy-moses/repos`)
+  .then(res => res.json())
+  .then((res: GithubRepo[]) => {
+      console.log(res);
+      return res
+  })
+};
 function App() {
+  const [data, setData] = useState<GithubRepo[]>([]);
+
+  useEffect(() => {
+    getData().then((res) => {
+      setData(res);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <AppBar style={{ background: '#5c6ae6', height: 85 }}>
+        <Toolbar>
+          <Typography variant="h4" color="white">
+            Репозитории пользователя:
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Grid style={{ marginTop: 100 }}
+        id="cards"
+        sx={{
+          margin: 5,
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center"
+        }}>
+        {data.map((item) => (
+          <Grid item xs={4}>
+            <Card key={item.id} sx={{
+              backgroundColor: "#5c6ae6",
+              color: "white",
+              height: 180,
+              margin: 3
+            }}>
+              <CardContent>
+                <Typography variant="h5" component="div" textAlign="center"> {item.name} </Typography>
+                <Typography color="white">
+                  -----------------------------------------------------------------------------------
+                </Typography>
+                <Typography variant="h6" color="white">
+                  {item.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+
+      </Grid>
+    </>
+    );
 }
 
 export default App;
